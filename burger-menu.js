a8e181ebc9c8af695e7c54aebab81d93c3a8f810
Aarav Batra <aaravbatra@duck.com>
2025-12-27 18:50:58 -0500

    remove burger menu

--- burger-menu.js
+++
@@ -1,6 +0,0 @@
-const burger = document.querySelector('.navbar-burger');
-const menu = document.querySelector('.navbar-items-c');
-
-burger.addEventListener('click', () => {
-    menu.classList.toggle('active');
-});
